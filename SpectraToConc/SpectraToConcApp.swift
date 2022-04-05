f9b408dbc11bf72950be29739687fd173a6a944f
Collin Miller <collinmiller@mines.edu>
2022-04-05 14:34:43 -0600

    Initial Commit

---
+++ SpectraToConc/SpectraToConcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectraToConcApp.swift
+//  SpectraToConc
+//
+//  Created by Miller on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SpectraToConcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
